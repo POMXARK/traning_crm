6560e7cd79afe8bc65097bf80e43c256fa56f2b6
roman <1997pom@gmail.com>
2023-11-28 23:46:37 +0400

    add timer audio signal

--- packages/CRM/Admin/src/Resources/views/training/training_plan/view.blade.php
+++ packages/CRM/Admin/src/Resources/views/training/training_plan/view.blade.php
@@ -3,7 +3,12 @@
 
 @section('content-wrapper')
 
-<display-field :data='@json($person)'></display-field>
+<display-field :data='@json($person)'
+               :rest='"{{ asset('vendor/CRM/admin/assets/rest.mp3') }}"'
+               :start='"{{ asset('vendor/CRM/admin/assets/start.mp3') }}"'
+               :pause='"{{ asset('vendor/CRM/admin/assets/pause.mp3') }}"'
+
+></display-field>
 
 {{--{{$person->approach_time}}--}}
 
@@ -20,32 +25,39 @@
             <div class="content full-page dashboard">
                 <div class="dashboard-content">
                     <div class="row-grid-3">
-            <div class="timer card">
-                <div class="card-data display-grid">
-                    <label class="card-header">@{{ data.name }}</label>
-                    <div class="column-container">
-                        <span></span>
-                        <span>Описание: @{{ data.description }}</span>
-                    </div>
-                    <div class="column-container">
-                        <span></span>
-                        <span>Количество повторений: @{{ data.number_repetitions }}</span>
-                    </div>
-                    <div class="column-container">
-                        <span></span>
-                        <span>Количество подходов: @{{ data.number_approaches }}</span>
-                    </div>
-                    <div class="column-container">
-                        <span></span>
-                        <span>@{{ minutes }} секунд</span>
+                        <div class="timer card">
+
+                            <audio ref="rest" :src="rest" ></audio>
+                            <audio ref="start" :src="start" ></audio>
+                            <audio ref="pause" :src="pause" ></audio>
+{{--                            <audio ref="audio" :src="audioSrc" controls></audio>--}}
+
+                            <div class="card-data display-grid">
+                                <label class="card-header">@{{ data.name }}</label>
+                                <div class="column-container">
+                                    <span></span>
+                                    <span>Описание: @{{ data.description }}</span>
+                                </div>
+                                <div class="column-container">
+                                    <span></span>
+                                    <span>Количество повторений: @{{ data.number_repetitions }}</span>
+                                </div>
+                                <div class="column-container">
+                                    <span></span>
+                                    <span>Количество подходов:   @{{ data.number_approaches }}</span>
+                                </div>
+                                <div class="column-container">
+                                    <span></span>
+                                    <span>@{{ minutes }} секунд</span>
+                                </div>
+                            </div>
+                            <br>
+                            <button type="submit" @click="startTimer" class="btn btn-sm btn-secondary-outline">
+                                Запустить таймер
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <br>
-                <button type="submit" @click="startTimer" class="btn btn-sm btn-secondary-outline">
-                    Запустить таймер
-                </button>
-            </div>
-            </div></div>
             </div>
         </script>
 
@@ -54,18 +66,20 @@
 
                 template: '#display-field-template',
 
-                props: ['data'],
+                props: ['data', 'rest', 'start', 'pause'],
 
                 data() {
                     return {
                         number_repetitions: this.data.number_repetitions,
                         minutes: '0:00',
                         time: 0,
-                        data: []
+                        data: [],
+                        audioSrc: null,
                     };
                 },
                 methods: {
                     startTimer() {
+                        this.playAudio('start');
                         this.approachTime(this.data.approach_time, 'run')
                     },
                     approachTime(time, action) {
@@ -75,24 +89,33 @@
                         const interval = setInterval(() => {
                             this.time--;
                             this.secondsToMMSS(this.time, action)
-                            if (this.data.number_repetitions === 0){
+                            if (this.data.number_repetitions === 0) {
                                 clearInterval(interval);
+                                this.playAudio('rest');
                                 if (this.data.number_approaches === 0) {
+                                    this.playAudio('pause');
                                     alert('Время вышло!');
                                     clearInterval(interval);
-                                } else {20
+                                } else {
                                     this.data.number_approaches--;
                                     this.data.number_repetitions = this.number_repetitions;
-                                    this.approachTime(this.data.rest_between_approaches, 'pause')
+                                    this.approachTime(this.data.rest_between_approaches, 'rest')
                                 }
                             }
                             else if (this.time === 0 && action === 'run') {
                                 clearInterval(interval);
+                                this.playAudio('pause');
                                 this.approachTime(this.data.pause_time, 'pause')
                                 this.data.number_repetitions--;
                             }
                             else if (this.time === 0 && action === 'pause') {
                                 clearInterval(interval);
+                                this.playAudio('start');
+                                this.approachTime(this.data.approach_time, 'run')
+                            }
+                            else if (this.time === 0 && action === 'rest') {
+                                clearInterval(interval);
+                                this.playAudio('start');
                                 this.approachTime(this.data.approach_time, 'run')
                             }
                         }, 1000);
@@ -120,7 +143,34 @@
                          if (action === 'pause') {
                              this.minutes = 'Время отдыха: ' + minutes + ':' + seconds;
                          }
-                     }
+                         if (action === 'rest') {
+                             this.minutes = 'Время перерыва: ' + minutes + ':' + seconds;
+                         }
+                     },
+                    playAudio(ref) {
+                        let audio = null;
+                        switch (ref) {
+                            case 'rest': {
+                                audio = this.$refs.rest;
+                                break;
+                            }
+                            case 'start': {
+                                audio = this.$refs.start;
+                                break;
+                            }
+                            case 'pause': {
+                                audio = this.$refs.pause;
+                                break;
+                            }
+                        }
+
+                        if (audio.paused) {
+                            audio.play();
+                        } else {
+                            audio.pause();
+                            audio.currentTime = 0;
+                        }
+                    },
                 },
 
             });
